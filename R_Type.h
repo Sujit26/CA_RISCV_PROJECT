16f3ac510e0e0ba4ecc5ba042fe959757c1e31bb
harshagarg09 <39459950+harshagarg09@users.noreply.github.com>
2020-03-23 21:51:59 +0530

    Update R_Type.h

--- R_Type.h
+++ R_Type.h
@@ -118,13 +118,7 @@ class RType
 			bitset<5> rd(parameter[0]);
 			bitset<5> rs1(parameter[1]);
 			bitset<5> rs2(parameter[2]);
-			if(parameter.size()>3)
-			{
-				for(int i=0;i<32;i++)
-					Machinecode[i]=-1;
-				return Machinecode;
-			}
-			if(parameter[0]<0 || parameter[0]>32 || parameter[1]<0 || parameter[1]>32 || parameter[2]<0 || parameter[2]>32 || )
+			if(parameter.size()!=3 || parameter[0]<0 || parameter[0]>32 || parameter[1]<0 || parameter[1]>32 || parameter[2]<0 || parameter[2]>32  )
 			{
 				for(int i=0;i<32;i++)
 					Machinecode[i]=-1;
