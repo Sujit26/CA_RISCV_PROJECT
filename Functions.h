ca834c463ac38cf5d0db71ae1b992637e62252ef
Lakshya <45898825+heylakshya@users.noreply.github.com>
2020-03-23 19:48:36 +0530

    Update Functions.h
    updated

--- Functions.h
+++ Functions.h
@@ -1,17 +1,14 @@
-#pragma once
-#include<bitset>
-using namepsace std;
 
-template<size_t BIT_SIZE>int bitsetRead(bitset<BIT_SIZE>inpBits)
-{
-	if(inpBits[BIT_SIZE-1]==1)
-	{
+#pragma once
+#include <bitset>
+using namespace std;
+
+
+template <size_t BIT_SIZE> int bitsetRead (bitset <BIT_SIZE> inpBits ) {
+	if (inpBits[BIT_SIZE - 1] == 1) {
 		inpBits = inpBits.flip();
-		return (-1 * inpBits.to_ulong())-1;
-	}
-	else
-	{
+		return  (-1 * inpBits.to_ulong()) - 1;
+	} else {
 		return inpBits.to_ulong();
 	}
-
-}
+} 
