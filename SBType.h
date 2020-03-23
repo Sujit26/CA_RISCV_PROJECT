b13a0e6a3d969cacd5a113e55ae9667bd8c35bd2
harshagarg09 <39459950+harshagarg09@users.noreply.github.com>
2020-03-24 00:04:41 +0530

    Update SBType.h

--- SBType.h
+++ SBType.h
@@ -8,66 +8,87 @@
 
 using namespace std;
 
-class SType{
-private:
-    vector <string> instructions;
-    int error = 0;
-// instruction : [ immediate (7) ][ rs2 (5)][rs1 (5)][func3][immediate (5)][opcode (7)]
-    vector <string> opcode;
-    vector <string> funct3;
-    // extract all integers from givenn isntrustion(str)
-    // for eg sw x3 0(x7)   extracted 3 0 7 
-    vector<int> extractint(string str){
-        vector <int> result;
-        int sum,currentint;
-        int counter = 0;
+class SBType {
+    // instruction :-  [ imm[12] ][ imm [10:5] ][ rs2 ][ rs1 ][ funct3 ][ imm[4:1] ][ imm[11] ][ opcode ]
+    vector<string> instructions;
+    vector<string> opcode;
+    vector<string> funct3;
+              int error = 0;
+    // eg beq x0 x1 number 
+    /* Function extracts the all the integers in an instruction basically */
+    vector <int> extractint(string str) { 
+        // countCommas counts the commas to ensure no numbers are picked off the LABEL.
+        vector <int> final_result;
+        int count=0;
+        int sum,currentint, sum2;
         for(int strIndex = 0 ; strIndex < str.size() ; strIndex++) {
             
             sum = 0;
-            bool intfound = 0;
+            bool intfound = 0, intfound2 = 0;
 
             while(strIndex < str.size() && isdigit(str[strIndex])) {
-                if(counter==2)
+                if(count==2)
                 {
-                    if(isalpha(str[strIndex]))
-                        error = -1;
+                    if(str[strIndex-1]=='x')
+                    error =-1;
                 }
                 currentint = str[strIndex] - '0';
                 sum = sum*10 + currentint;
                 strIndex++;
                 intfound = 1;
             }
+
+            if(str[strIndex] == '-'){
+                // for negative values
+                if(count==2)
+                {
+                    if(str[strIndex-1]=='x')
+                    error =-1;
+                }
+                sum2 = 0;
+                strIndex++;
+                while(strIndex < str.size() && isdigit(str[strIndex])){
+                    currentint = str[strIndex] - '0';
+                    sum2 = sum2*10 + currentint;
+                    strIndex++;
+                    intfound2 = 1;
+                }
+                sum2 = sum2*(-1);
+            }
             
             if(intfound){
-                counter = counter +1;
-                result.push_back(sum);}
+                count = count+1;
+                final_result.push_back(sum);}
+            
+            if(intfound2){
+                count = count+1;
+                final_result.push_back(sum2);}
         }
 
-        return result;
+        return final_result; //returning vector of extracted parameters
     }
-public:
+
+
+    public:
+
     // initialise the vectors with their respective values from the input file.
     void initialise (string filename) {
         ifstream ifile(filename.c_str());
         string line;
-        while (getline(ifile,line)) {
+        while(getline(ifile,line)) {
             stringstream ss(line);
-            string token; // temp string named as token
-            
+            string token;
             ss >> token;
-            instructions.push_back(token); // putting instrucion name in our database
-            
+            instructions.push_back(token);
             ss >> token;
-            opcode.push_back(token); // putting opcode to  respective instrucion name in our database
-            
+            opcode.push_back(token);
             ss >> token;
-            funct3.push_back(token); // putting function3 to  respective instrucion name in our database
+            funct3.push_back(token);
         }
     }
 
-    // tell weather given command is present in our database or not
     bool isPresent(string command)
-    {   
+    {
         stringstream ss(command);
         string ins;
         ss >> ins;
@@ -77,19 +98,16 @@ public:
         else
         return true;
     }
-    
+
     bitset <32> decode (string instruction) {
-        bitset <32> MC;
+        bitset <32> MachineCode;
         stringstream ss(instruction); //helpful for tokenizing space separated strings.
         vector <int> parameters = extractint(instruction); // extracted all register names, offsets etc.
-        
-        if(parameters.size()!=3||error==-1)
+        if(parameters.size()!=3||error==-1||parameters[0]>31||parameters[0]<0||parameters[1]<0||parameters[1]>31)
         {
             for(int i=0;i<32;i++)
-                MC[i]=-1;
-            
-                error = 0;
-                return MC;
+                MachineCode[i]=-1;
+            return MachineCode;
         }
         string action;
         ss >> action;
@@ -98,36 +116,33 @@ public:
         
         opcodestr = opcode[index];
         funct3str = funct3[index];
-        bitset <12> immediate(parameters[2]); // loading offset
-        bitset <5> rs1(parameters[1]),rs2(parameters[0]);
-        if(parameters[2]<-2048||parameters[2]>2047 || parameters[0]<0 || parameters[0]>31 || parameters[1]<0 || parameters[1]>31)
-			{
-				for(int i=0;i<32;i++)
-					MC[i]=-1;
-				return MC;
-			}
-        for(int i=0;i<7;i++){
-            if(opcodestr[opcodestr.size()-1-i] == '0')
-                MC[i] = 0;
-            else 
-                MC[i] = 1; //copying opcode string to the opcode field
-        }
-        for(int i=0;i<5;i++)
-            MC[i+7] = immediate[i];
+        bitset <12> immediate(parameters[2]);//The label offset has been taken care of
+        bitset <5> rs1(parameters[0]),rs2(parameters[1]);
 
-         for(int i = 0; i<3; i++)
-            MC[i+12] = (funct3str[funct3str.size()-i-1] == '0') ? 0 : 1;
+        for(int i=0;i<7;i++)
+            MachineCode[i] = (opcodestr[opcodestr.size()-1-i] == '0') ? 0 : 1; //copying opcode string to the opcode field
         
+        MachineCode[7] = immediate[10];
+
+        for(int i=0;i<4;i++)
+            MachineCode[i+8] = immediate[i];
+
+        
+        for(int i = 0; i<3; i++)
+            MachineCode[i+12] = (funct3str[funct3str.size()-i-1] == '0') ? 0 : 1;
+
         for(int i=0;i<5;i++)
-            MC[i+15] = rs1[i];
+            MachineCode[i+15] = rs1[i];
         
         for(int i=0; i<5 ; i++)
-            MC[i+20] = rs2[i];
+            MachineCode[i+20] = rs2[i];
         
-        for(int i=0;i<7;i++)
-            MC[i+25] = immediate[i+5];
+        for(int i=0;i<6;i++)
+            MachineCode[i+25] = immediate[i+4];
+        
+        MachineCode[31] = immediate[11];
 
-        return MC;
+        return MachineCode;
 
     }
 };
