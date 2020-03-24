5a6a53a0e950a084bf1651100c56f894eb6b1ffa
Sujit26 <45810709+Sujit26@users.noreply.github.com>
2020-03-24 15:36:30 +0530

    Update ALU with printing on terminal

--- ALU.h
+++ ALU.h
@@ -29,43 +29,54 @@ class ALU {
 		if(ins == "add"){
 			result = RA + RB;
 			object.RZ.writeInt(result);
+			cout <<"\t\t"<<result <<"="<< RA <<"+"<< RB<<endl;
 		}
 		else if(ins == "sub"){
 			result = RA - RB;
+			cout <<"\t\t"<<result <<"="<< RA <<"-"<< RB<<endl;
 			object.RZ.writeInt(result);
 		}
 		else if(ins == "mul"){
 			result = RA * RB;
+			cout <<"\t\t"<<result <<"="<< RA <<"*"<< RB<<endl;
 			object.RZ.writeInt(result);
 		}
 		else if(ins == "div"){
 			result = RA / RB;
+			cout <<"\t\t"<<result <<"="<< RA <<"/"<< RB<<endl;
 			object.RZ.writeInt(result);
 		}
 		else if(ins == "rem"){
 			result = RA % RB;
+			cout <<"\t\t"<<result <<"="<< RA <<"%"<< RB<<endl;
 			object.RZ.writeInt(result);
 		}
 		else if(ins == "and"){
 			result = RA & RB;
+			cout <<"\t\t"<<result <<"="<< RA <<" and "<< RB<<endl;
 			object.RZ.writeInt(result);
 		}
 		else if(ins == "or"){
 			result = RA | RB;
+			cout <<"\t\t"<<result <<"="<< RA <<" or "<< RB<<endl;
 			object.RZ.writeInt(result);
 		}
 		else if(ins == "xor"){
 			result = RA ^ RB;
+			cout <<"\t\t"<<result <<"="<< RA <<" xor "<< RB<<endl;
 			object.RZ.writeInt(result);
 		}
 		else if(ins == "sll"){
 			result = RA << RB ;
+			cout <<"\t\t"<<result <<"="<< RA <<" << "<< RB<<endl;
 			object.RZ.writeInt(result);
 		}
 		else if(ins == "srl"){
 			result = RA >> RB;
+			cout <<"\t\t"<<result <<"="<< RA <<" >> "<< RB<<endl;
 			object.RZ.writeInt(result);
 		}
+		
 		else if(ins == "blt"){
 			state = (RA < RB) ? 1 : 0;
 		}
