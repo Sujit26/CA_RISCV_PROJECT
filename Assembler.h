10b58587b8566e438532608946d4139db880c098
Sujit26 <45810709+Sujit26@users.noreply.github.com>
2020-03-27 13:25:37 +0530

    Final Edit For Phase 2

--- Assembler.h
+++ Assembler.h
@@ -1,105 +1,237 @@
+// Handling assembler directives
 #include <iostream>
-#include <vector>
 #include <string>
 #include <fstream>
-#include <bitset>
-#include <algorithm>
-#include <ctype.h>
+#include <sstream>
+#include <vector>
+#include <stdlib.h>
+#include "MemoryAccess.h"
 
 using namespace std;
 
-class UType{
-		//instuction imm[31:12] | rd[11:7] | opcode[6:0]	
-	private:
-		vector <string> instructions;
-    vector <string> opcode;
-	
-	public:
-		// initialise the vectors with their respective values from the input file.
-    void initialise (string filename);
-		// checks if given command is present in the list of S Type instructions.
-    bool isPresent(string command);
-		// Converts string instruction to 32 bit machine code
-		bitset <32> decode (string instruction);
-};
+vector <int> extractint(string str) { // recieves a string and extracts all the integers and returns them in a list (vector)
+        vector <int> result;
+        int sum,currentint;
+        bool valid = 0;
+        for(int strIndex = 0 ; strIndex < str.size() ; strIndex++) {
+            
+            sum = 0;
+            bool intfound = 0;
+            
+            while(strIndex < str.size() && isdigit(str[strIndex]) && valid) {
+                    currentint = str[strIndex] - '0';
+                    sum = sum*10 + currentint;
+                    strIndex++;
+                    intfound = 1;
+            }
+            
+            if(intfound)
+                result.push_back(sum);
 
-void UType::initialise (string filename) {
-	ifstream ifile(filename.c_str());
-  string instName,instOpcode;
-  while(ifile >> instName >> instOpcode) {
-    instructions.push_back(instName);
-    opcode.push_back(instOpcode);
-  }
-}
+            if(str[strIndex] == ':'){
+                valid = 1;
+            }
+        }
 
-bool UType::isPresent(string inst){
-	int i=0;
-	string instName;
-	while(!isalnum(inst[i])) i++;
-	while(isalpha(inst[i])){
-	 	instName.push_back(inst[i]);
-		i++;
-	}
-	vector <string> :: iterator itr = find(instructions.begin(),instructions.end(),instName);
-	if(itr == instructions.end())	return false;
-	return true;
-}
+        return result; //returning vector of extracted parameters
+    }
 
-bitset <32> UType::decode (string instruction){
-	bitset <32> machineCode;
-	string instName, rdName, immStr;
+vector <int> extractasc(string str) { // recieves a string and extracts all the integers and returns them in a list (vector)
+        vector <int> result;
+        bool valid1 = 0,exitint = 0;
+        for(int strIndex = 0 ; strIndex < str.size() && !exitint; strIndex++) {
+            while(valid1) {
+                if(str[strIndex] == '"'){
+                    exitint = 1;
+                    break;
+                }
+                result.push_back(int(str[strIndex]));
+                strIndex++;
+            }
 
-	// Splitting instruction into various parts, ignoring whitespaces and commas
-	int rdNum=0,immValue=0,i=0;
-	while(!isalnum(instruction[i])) i++;
-	while(isalnum(instruction[i])){
-	 	instName.push_back(instruction[i]);
-		i++;
-	}
-	while(!isalnum(instruction[i])) i++;
-	while(isalnum(instruction[i])){
-	 	rdName.push_back(instruction[i]);
-		i++;
-	}
-	while(!isalnum(instruction[i])) i++;
-	while(isalnum(instruction[i])){
-	 	immStr.push_back(instruction[i]);
-		i++;
-	}
+            if(str[strIndex] == '"'){
+                valid1 = 1;
+            }
+        }
+        return result; //returning vector of extracted parameters
+    }
 
-	// index for opcode
-	int index = find(instructions.begin(),instructions.end(),instName) - instructions.begin();
-	for(int i=0; i<7; i++)
-		machineCode[i]=opcode[index][6-i]-'0';
+void  assembler_initiate(MemoryAccess &memobject)
+{
+    ifstream ifile("input.txt");
+    ofstream ofile("input1.txt",ios::out);
+    string current;
+     // matches the label to address in the memory or value it is referring to.
+    map <string , int > labelLookup;
+    bool start = 0;
+    bool starttext = 0;
+    bool labeldef = 0 ;
+    int address =  0;
+    
+    while(getline(ifile,current))
+    {
+        bool useful = false;
+        for(int i = 0 ; i < current.size() ; i++)
+        {
+            if(isalpha(current[i]))
+                useful = 1;
+        }
+        
+        if(!useful){
+            continue;
+        }
+        
+        if(current == ".data"){
+            start = 1;
+            continue;
+        }
+        else if(current.size() == 0){
+            continue;
+        }
+        else if(current == ".text"){
+            starttext = 1;
+            start = 0;
+            continue;
+        }
+        else if(start == 1){ //.data portion has started 
+            stringstream ss(current);
+            vector <int> data;
+            bool Word = 0, byte = 0, half=0 ,dword=0, asciiz = 0;
+            string token;
+            string directive;
+            ss >> token;
+            ss >> directive;
+            if(directive == ".word"){
+                Word = true;
+            }
+            else if(directive == ".byte"){
+                byte = true;
+            }
+            else if(directive == ".half"){
+                half = true;
+            }
+            else if(directive == ".dword"){
+                dword = true;
+            }
+            else if(directive == ".asciiz"){
+                asciiz = true;
+            }
 
-	//Finding register number
-	if(rdName.size()==2)
-		rdNum = rdName[1]-'0';
-	else
-		rdNum = 10*(rdName[1]-'0') + (rdName[2]-'0');
-	bitset <5> rd(rdNum);
-	
-	//Generating immediate value
-	if(immStr[0]=='0' && immStr[1]=='x'){ // Hexdecimal input
-		for(i=2;i<immStr.size();i++){
-			if(immStr[i]>='a' && immStr[i]<='f')
-				immValue = immValue*16 + (immStr[i]-'a' + 10);
-			else if(immStr[i]>='A' && immStr[i]<='F')
-				immValue = immValue*16 + (immStr[i]-'A' + 10);
-			else
-				immValue = immValue*16 + (immStr[i]-'0');
-		}	
-	}
-	else{ // Decimal input
-		for(i=0;i<immStr.size();i++)
-				immValue = immValue*10 + (immStr[i]-'0');
-	}
-	bitset <20> imm(immValue);
-	
-	//Generating machine code
-	for(int i=0; i<5; i++)
-		machineCode[7+i] = rd[i];
-	for(int i=0; i<20; i++)
-		machineCode[12+i] = imm[i];
-	return machineCode;
+                if(token[token.size()-1] == ':'){
+                    labeldef = 1;
+                    token.pop_back();
+                    if(!asciiz)
+                        data = extractint(current);
+                    else
+                        data = extractasc(current);
+                }
+            //write the array to memory  (if at all it is an array ). , else write the single value or byte.
+            
+            if(labeldef){
+                if(data.size() > 1){
+                    labelLookup.insert(make_pair(token,address));
+                }
+                else{
+                    labelLookup.insert(make_pair(token,data[0]));
+                }
+                if(Word == true){
+                    for(int i=0; i < data.size(); i++){
+                        memobject.writeWord(address,data[i]); //Word $$$$$
+                        address += 4;
+                    }
+                }
+                else if(byte == true){
+                    for(int i=0; i < data.size(); i++){
+                        memobject.writeByte(address,data[i]); //byte $$$$$
+                        address += 1;
+                    }
+                }   else if(half == true){
+                    for(int i=0; i < data.size(); i++){
+                        memobject.writeByte(address,data[i]); //half $$$$$
+                        address += 2;
+                    }
+                }   else if(dword == true){
+                    for(int i=0; i < data.size(); i++){
+                        memobject.writeByte(address,data[i]); //dword $$$$$
+                        address += 8;
+                    }
+                }   else if(asciiz == true){
+                    cout<<"CHECK\n";
+                    for(int i=0; i < data.size(); i++){
+                        // just assingning ascii value of that char, we will handle it while reading
+                        memobject.writeByte(address,data[i]); //ascii $$$$$
+                        address += 1;
+                    }
+                }
+            }
+            
+            
+            labeldef = 0;
+        }
+        else if(starttext == 1){
+            string insname,regname,label;
+            int i = 0;
+            
+            while(!isalnum(current[i])) i++;
+            while(isalnum(current[i])){
+                insname.push_back(current[i]);
+                i++;
+            }
+            while(!isalnum(current[i])) i++;
+            while(isalnum(current[i])){
+                regname.push_back(current[i]);
+                i++;
+            }
+            while(!isalnum(current[i])) i++;
+            while( i < current.size() && current[i] != '#'){
+                if(current[i] == ' ' || current[i] == '\t'){
+                    i++;
+                    continue;
+                }
+                label.push_back(current[i]);
+                i++;
+            }
+            
+            
+            
+            if( (insname == "la" || insname == "lw" || insname == "lb") && label[label.size()-1] != ')' )
+            {
+                if(!isdigit(label[0]))
+                {
+                    ofile<<"addi "<<regname<<",x0,"<<labelLookup[label]<<endl;
+                    continue;
+                }  
+            }
+           
+            else
+            {
+                bool commline = 0;
+                for(int i = 0; i < current.size()  ; i++)
+                {
+                    if(current[i] == '#'){
+                        if(i == 0 )
+                            commline = 1;
+                        break;
+                    }
+                    ofile<<current[i];
+                    
+                }
+                if(!commline)
+                ofile<<endl;
+            }
+        }
+
+        if(!start && !starttext){
+                for(int i = 0; i < current.size() && current[i] != '#' ; i++)
+                {
+                    ofile<<current[i];
+                }
+                ofile<<endl;
+        }
+       
+    }
+
+    ofile.close();
+    ifile.close();
+    
 }
